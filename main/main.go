db6cdfafce875ab25f0a92565e91451fc45d0189
DilaraSeker <dlrseker@gmail.com>
2022-02-24 01:49:14 +0300

    Store data in slice and convert to json

--- main/main.go
+++ main/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"strconv"
 )
 
 func main() {
@@ -15,17 +17,31 @@ func main() {
 	}
 }
 
-func fizzbuzz(i int) {
+func fizzbuzz(input int) {
+
 	fizz := "fizz"
 	buzz := "buzz"
+	var output []string
+	for i := 1; i <= input; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			output = append(output, fizz+buzz)
+			fmt.Println(i, fizz+buzz)
+		} else if i%3 == 0 {
+			output = append(output, fizz)
+			fmt.Println(i, fizz)
+		} else if i%5 == 0 {
+			output = append(output, buzz)
+			fmt.Println(i, buzz)
+		} else {
+			output = append(output, strconv.Itoa(i))
+			fmt.Println(i)
+		}
+	}
+	jsonObj, err := json.Marshal(output)
 
-	if i%3 == 0 && i%5 == 0 {
-		fmt.Println(i, fizz+buzz)
-	} else if i%3 == 0 {
-		fmt.Println(i, fizz)
-	} else if i%5 == 0 {
-		fmt.Println(i, buzz)
+	if err != nil {
+		fmt.Printf("Error: %s", err.Error())
 	} else {
-		fmt.Println(i)
+		fmt.Printf("{\"fizzBuzz\": %s}", jsonObj)
 	}
 }
